10eb6e7340272daf03edc189c7abe56a1864baf6
Connor_Crump <ccrump@uoregon.edu>
2023-07-31 15:11:56 -0700

    added vite config for gh-pages

--- src/tests/SudokuBoard.test.tsx
+++ src/tests/SudokuBoard.test.tsx
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import SudokuBoard from '../components/sudokuBoard';
-import Game from '../gameLogic/game';
 
 test('Should render without error', () => {
-  const game = new Game("easy");
-  render(<SudokuBoard game={game} />);
+  render(<SudokuBoard/>);
   const appElement = screen.findByTestId("sudoku-board");
   expect(appElement).toBeTruthy;
 });
